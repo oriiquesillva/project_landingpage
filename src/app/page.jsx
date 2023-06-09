1cb103d687ba92f6b08957d96b758355913f7f50
Luiz Souza <lsillva.dev@gmail.com>
2023-06-09 08:46:07 -0300

    feat: configuring the project

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+
+import Header from '@/components/header/header'
+import Welcome from '@/components/welcome/welcome'
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <Welcome/>
+    </div>
+  )
+}
